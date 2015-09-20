d1e55eb7e7571a46be82518cf63c1343260a1064
Flyte <flyte@failcode.co.uk>
2015-09-20 21:35:46 +0100

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def required(fname):
 
 config = {
     "name": "django-condition-chain",
-    "version": "0.0.2",
+    "version": "0.0.3",
     "namespace_packages": [],
     "packages": find_packages(exclude=["*.tests",
                                        "*.tests.*",
